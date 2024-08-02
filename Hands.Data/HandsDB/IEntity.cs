48d4f880be4f29a614679022498e6f73f677dda7
kashiffareed <kashiffareed01@gmail.com>
2024-08-02 10:23:17 +0500

    initial commit

---
+++ Hands.Data/HandsDB/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hands.Data.HandsDB
+{
+    public interface IEntity
+    {
+
+          bool IsActive { get; set; } 
+    }
+}
